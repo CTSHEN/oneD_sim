957a9a8d49cf2c222fb8de4fda5160eb0c01f70e
CTSHEN <eric810819@hotmail.com>
2022-09-28 15:19:22 +0800

    3 states 2 control 1-D height control, no thruster dynamics.

--- oneD_sim_MultiphaseTest.cpp
+++ oneD_sim_MultiphaseTest.cpp
@@ -148,13 +148,13 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     // Boundary of phase 1 to 8: 3 state continuity + 1 time continuity
     // Total: 28 linkage constraints
 
-    adouble xf1[5], xi2[5], tf1, ti2; // P1 & P2
-    adouble xf2[5], xi3[5], tf2, ti3; // P2 & P3
-    adouble xf3[5], xi4[5], tf3, ti4; // P3 & P4
-    adouble xf4[5], xi5[5], tf4, ti5; // P4 & P5
-    adouble xf5[5], xi6[5], tf5, ti6; // P5 & P6
-    adouble xf6[5], xi7[5], tf6, ti7; // P6 & P7
-    adouble xf7[5], xi8[5], tf7, ti8; // P7 & P8
+    adouble xf1[3], xi2[3], tf1, ti2; // P1 & P2
+    adouble xf2[3], xi3[3], tf2, ti3; // P2 & P3
+    adouble xf3[3], xi4[3], tf3, ti4; // P3 & P4
+    adouble xf4[3], xi5[3], tf4, ti5; // P4 & P5
+    adouble xf5[3], xi6[3], tf5, ti6; // P5 & P6
+    adouble xf6[3], xi7[3], tf6, ti7; // P6 & P7
+    adouble xf7[3], xi8[3], tf7, ti8; // P7 & P8
 
     // Linking phase 1 and 2
     get_final_states(   xf1, xad, 1, workspace);
@@ -165,9 +165,7 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     linkages[0] = xf1[0] - xi2[0];
     linkages[1] = xf1[1] - xi2[1];
     linkages[2] = xf1[2] - xi2[2];
-    linkages[3] = xf1[3] - xi2[3];
-    linkages[4] = xf1[4] - xi2[4];
-    linkages[5] = tf1 - ti2;
+    linkages[3] = tf1 - ti2;
 
     // Linking phase 2 and 3
     get_final_states(   xf2, xad, 2, workspace);
@@ -175,12 +173,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf2 = get_final_time(    xad, 2, workspace);
     ti3 = get_initial_time(  xad, 3, workspace);
 
-    linkages[6] = xf2[0] - xi3[0];
-    linkages[7] = xf2[1] - xi3[1];
-    linkages[8] = xf2[2] - xi3[2];
-    linkages[9] = xf2[3] - xi3[3];
-    linkages[10] = xf2[4] - xi3[4];
-    linkages[11] = tf2 - ti3;
+    linkages[4] = xf2[0] - xi3[0];
+    linkages[5] = xf2[1] - xi3[1];
+    linkages[6] = xf2[2] - xi3[2];
+    linkages[7] = tf2 - ti3;
 
     // Linking phase 3 and 4
     get_final_states(   xf3, xad, 3, workspace);
@@ -188,12 +184,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf3 = get_final_time(    xad, 3, workspace);
     ti4 = get_initial_time(  xad, 4, workspace);
 
-    linkages[12] = xf3[0] - xi4[0];
-    linkages[13] = xf3[1] - xi4[1];
-    linkages[14] = xf3[2] - xi4[2];
-    linkages[15] = xf3[3] - xi4[3];
-    linkages[16] = xf3[4] - xi4[4];
-    linkages[17] = tf3 - ti4;
+    linkages[8] = xf3[0] - xi4[0];
+    linkages[9] = xf3[1] - xi4[1];
+    linkages[10] = xf3[2] - xi4[2];
+    linkages[11] = tf3 - ti4;
 
     // Linking phase 4 and 5
     get_final_states(   xf4, xad, 4, workspace);
@@ -201,12 +195,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf4 = get_final_time(    xad, 4, workspace);
     ti5 = get_initial_time(  xad, 5, workspace);
 
-    linkages[18] = xf4[0] - xi5[0];
-    linkages[19] = xf4[1] - xi5[1];
-    linkages[20] = xf4[2] - xi5[2];
-    linkages[21] = xf4[3] - xi5[3];
-    linkages[22] = xf4[4] - xi5[4];
-    linkages[23] = tf4 - ti5;
+    linkages[12] = xf4[0] - xi5[0];
+    linkages[13] = xf4[1] - xi5[1];
+    linkages[14] = xf4[2] - xi5[2];
+    linkages[15] = tf4 - ti5;
 
     // Linking phase 5 and 6
     get_final_states(   xf5, xad, 5, workspace);
@@ -214,12 +206,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf5 = get_final_time(    xad, 5, workspace);
     ti6 = get_initial_time(  xad, 6, workspace);
 
-    linkages[24] = xf5[0] - xi6[0];
-    linkages[25] = xf5[1] - xi6[1];
-    linkages[26] = xf5[2] - xi6[2];
-    linkages[27] = xf5[3] - xi6[3];
-    linkages[28] = xf5[4] - xi6[4];
-    linkages[29] = tf5 - ti6;
+    linkages[16] = xf5[0] - xi6[0];
+    linkages[17] = xf5[1] - xi6[1];
+    linkages[18] = xf5[2] - xi6[2];
+    linkages[19] = tf5 - ti6;
 
     // Linking phase 6 and 7
     get_final_states(   xf6, xad, 6, workspace);
@@ -227,12 +217,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf6 = get_final_time(    xad, 6, workspace);
     ti7 = get_initial_time(  xad, 7, workspace);
 
-    linkages[30] = xf6[0] - xi7[0];
-    linkages[31] = xf6[1] - xi7[1];
-    linkages[32] = xf6[2] - xi7[2];
-    linkages[33] = xf6[3] - xi7[3];
-    linkages[34] = xf6[4] - xi7[4];
-    linkages[35] = tf6 - ti7;
+    linkages[20] = xf6[0] - xi7[0];
+    linkages[21] = xf6[1] - xi7[1];
+    linkages[22] = xf6[2] - xi7[2];
+    linkages[23] = tf6 - ti7;
 
     // Linking phase 7 and 8
     get_final_states(   xf7, xad, 7, workspace);
@@ -240,12 +228,10 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
     tf7 = get_final_time(    xad, 7, workspace);
     ti8 = get_initial_time(  xad, 8, workspace);
 
-    linkages[36] = xf7[0] - xi8[0];
-    linkages[37] = xf7[1] - xi8[1];
-    linkages[38] = xf7[2] - xi8[2];
-    linkages[39] = xf7[3] - xi8[3];
-    linkages[40] = xf7[4] - xi8[4];
-    linkages[41] = tf7 - ti8;
+    linkages[24] = xf7[0] - xi8[0];
+    linkages[25] = xf7[1] - xi8[1];
+    linkages[26] = xf7[2] - xi8[2];
+    linkages[27] = tf7 - ti8;
 }
 
 ////////// Define main routine //////////
@@ -262,24 +248,24 @@ int main(void)
     printf("CONFIG DONE!");
 
     // Rigister problem name //
-    problem.name = "1-D Simulation 2phase";
-    problem.outfilename = "1-d_sim_2phase.txt";
+    problem.name = "1-D Simulation Multuphase";
+    problem.outfilename = "1-d_sim_Multuphase.txt";
 
     // Define problem level constants & do level 1 setup //
     problem.nphases = TOTAL_PHASE;
-    problem.nlinkages = 42;
+    problem.nlinkages = 28;
 
     psopt_level1_setup(problem);
 
     // Define phase related information & do level 2 setup //
 
-    problem.phases(1).nstates = 5;
+    problem.phases(1).nstates = 3;
     problem.phases(1).ncontrols = 2;  
-    problem.phases(1).nevents = 5;
+    problem.phases(1).nevents = 3;
     problem.phases(1).npath = 0;
     problem.phases(1).nodes << TOTAL_NODE;
 
-    problem.phases(2).nstates = 5;
+    problem.phases(2).nstates = 3;
     problem.phases(2).ncontrols = 2;  
     problem.phases(2).nevents = 1;
     problem.phases(2).npath = 0;
@@ -287,14 +273,14 @@ int main(void)
 
     for(PhaseIdx=3;PhaseIdx<8; PhaseIdx++ )
     {
-        problem.phases(PhaseIdx).nstates = 5;
+        problem.phases(PhaseIdx).nstates = 3;
         problem.phases(PhaseIdx).ncontrols = 2;  
         problem.phases(PhaseIdx).nevents = 0;
         problem.phases(PhaseIdx).npath = 0;
         problem.phases(PhaseIdx).nodes << TOTAL_NODE;
     }
 
-    problem.phases(8).nstates = 5;
+    problem.phases(8).nstates = 3;
     problem.phases(8).ncontrols = 2;  
     problem.phases(8).nevents = 0;//2;
     problem.phases(8).npath = 0;
@@ -304,14 +290,14 @@ int main(void)
 
     ////////// Problem bounds information //////////
     // Phase 1
-    problem.phases(1).bounds.lower.states << 0, -2, 0, 0, 0;
-    problem.phases(1).bounds.upper.states << 3, 2, MF_INIT, 20, 15;
+    problem.phases(1).bounds.lower.states << 0, -2, 0;
+    problem.phases(1).bounds.upper.states << 3, 2, MF_INIT;
 
     problem.phases(1).bounds.lower.controls << 1.0, 0.0;
     problem.phases(1).bounds.upper.controls << 1.0, 0.0;
 
-    problem.phases(1).bounds.lower.events << 0, 0, MF_INIT, 0, 0;
-    problem.phases(1).bounds.upper.events << 0, 0, MF_INIT, 0, 0;
+    problem.phases(1).bounds.lower.events << 0, 0, MF_INIT;
+    problem.phases(1).bounds.upper.events << 0, 0, MF_INIT;
 
     problem.phases(1).bounds.lower.StartTime = 0.0;
     problem.phases(1).bounds.upper.StartTime = 0.0;
@@ -321,8 +307,8 @@ int main(void)
 
     for(PhaseIdx=3; PhaseIdx<=7; PhaseIdx = PhaseIdx+2)
     {
-        problem.phases(PhaseIdx).bounds.lower.states << 0, -2, 0, 0, 0;
-        problem.phases(PhaseIdx).bounds.upper.states << 3, 2, MF_INIT, 20, 15;
+        problem.phases(PhaseIdx).bounds.lower.states << 0, -2, 0;
+        problem.phases(PhaseIdx).bounds.upper.states << 3, 2, MF_INIT;
         problem.phases(PhaseIdx).bounds.lower.controls << 1.0, 0.0;
         problem.phases(PhaseIdx).bounds.upper.controls << 1.0, 0.0;
         problem.phases(PhaseIdx).bounds.lower.StartTime = 0.0;
@@ -333,8 +319,8 @@ int main(void)
 
     for(PhaseIdx=2; PhaseIdx<=6; PhaseIdx = PhaseIdx+2)
     {
-        problem.phases(PhaseIdx).bounds.lower.states << 0, -2, 0, 0, 0;
-        problem.phases(PhaseIdx).bounds.upper.states << 3, 2, MF_INIT, 20, 15;
+        problem.phases(PhaseIdx).bounds.lower.states << 0, -2, 0;
+        problem.phases(PhaseIdx).bounds.upper.states << 3, 2, MF_INIT;
         problem.phases(PhaseIdx).bounds.lower.controls << 0.0, 1.0;
         problem.phases(PhaseIdx).bounds.upper.controls << 0.0, 1.0;
         problem.phases(PhaseIdx).bounds.lower.StartTime = 0.0;
@@ -347,8 +333,8 @@ int main(void)
     problem.phases(2).bounds.upper.events << 0;
 
     // Phase 8
-    problem.phases(8).bounds.lower.states << 0, -2, 0, 0, 0;
-    problem.phases(8).bounds.upper.states << 3, 2, MF_INIT, 20, 15;
+    problem.phases(8).bounds.lower.states << 0, -2, 0;
+    problem.phases(8).bounds.upper.states << 3, 2, MF_INIT;
 
     problem.phases(8).bounds.lower.controls << 0.0, 1.0;
     problem.phases(8).bounds.upper.controls << 0.0, 1.0;
@@ -373,13 +359,12 @@ int main(void)
     ////////// Initial guess //////////
     int nnodes= problem.phases(1).nodes(0);
 
-    MatrixXd x0(5, nnodes); //TRY
+    MatrixXd x0(3, nnodes); //TRY
 
     x0.row(0) = linspace(0.1,1.0, nnodes);
     x0.row(1) = linspace(0.1,1.0, nnodes);
     x0.row(2) = linspace(0.6,0.1, nnodes);
-    x0.row(3) = linspace(0,0, nnodes);
-    x0.row(4) = linspace(0,0, nnodes);
+    
 
     //problem.phases(1).guess.controls       = zeros(2,nnodes);
     for(PhaseIdx=1; PhaseIdx<=8; PhaseIdx++)
@@ -445,7 +430,7 @@ int main(void)
     MatrixXd uPhase8        = solution.get_controls_in_phase(8);
     MatrixXd tPhase8 		= solution.get_time_in_phase(8);
 
-    MatrixXd xStar(5, length(tPhase1)+length(tPhase2)+length(tPhase3)
+    MatrixXd xStar(3, length(tPhase1)+length(tPhase2)+length(tPhase3)
         +length(tPhase4)+length(tPhase5)+length(tPhase6)+length(tPhase7)+length(tPhase8));
     xStar << xPhase1, xPhase2, xPhase3, xPhase4, xPhase5, xPhase6, xPhase7, xPhase8;
     MatrixXd uStar(2, length(tPhase1)+length(tPhase2)+length(tPhase3)
@@ -488,29 +473,15 @@ int main(void)
 
     ////////// Plot the result //////////
     plot(tStar,xStar.block<3,TOTAL_NODE*TOTAL_PHASE>(0,0),problem.name + ": states", "time (s)", "states", "x v m ");
-    plot(tStar,xStar.block<2,TOTAL_NODE*TOTAL_PHASE>(3,0),problem.name + ": states", "time (s)", "states", "fd fu");
-
     //plot(interp_time, interp_state, problem.name + ": interp states", "time (s)", "interp states", "x"); // FOR INTERPOLATE TEST
 
-    plot(tStar,uStar,problem.name + ": control", "time (s)", "control", "control");
+    multiplot(tStar,uStar,problem.name + ": control", "time (s)", "control", "ThrusterDown ThrusterUp", 2, 1);
 
     //plot(t,H,problem.name + ": Hamiltonian", "time (s)", "H", "H");
 
     //plot(t,lambda,problem.name + ": costates", "time (s)", "lambda", "lambda_1 lambda_2 lambda_3");
 
-    /*
-    plot(t,xStar,problem.name + ": states", "time (s)", "states", "x y v",
-                                  "pdf", "brac1_states.pdf");
-
-    plot(t,uStar,problem.name + ": control", "time (s)", "control", "u",
-                              "pdf", "brac1_control.pdf");
-
-    plot(t,H,problem.name + ": Hamiltonian", "time (s)", "H", "H",
-                                  "pdf", "brac1_hamiltonian.pdf");
-
-    plot(t,lambda,problem.name + ": costates", "time (s)", "lambda", "lambda_1 lambda_2 lambda_3",
-                                  "pdf", "brac1_costates.pdf");
-    */
+    
 
 
 
